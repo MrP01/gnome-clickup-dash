acc14bd5f5da9c9a1c768360df5cb8df6597c48a
MrP01 <peter@waldert.at>
2021-12-23 17:14:01 +0100

    add tasks-per-day display

--- clickup.py
+++ clickup.py
@@ -4,11 +4,20 @@ import json
 import os
 import sys
 
+import pytz
 import requests
-import termplot
+
+import models
 
 with open(os.path.expanduser("~/.config/gnome-clickup-dash/clickup.config.json")) as f:
     config = json.load(f)
+
+OFFSET_INTO_NEW_DAY = datetime.timedelta(hours=4, minutes=30)
+DAYS_BACK = 10
+
+
+def points_to_string(points: float):
+    return str(int(points) if int(points) == points else points)
 
 
 def fetch():
@@ -18,37 +27,54 @@ def fetch():
             f"https://api.clickup.com/api/v2/view/{workspace['still_due_view_id']}/task",
             headers={"Authorization": config["api_token"]}, data={"page": 0}
         )
+        print(".. Workspace requested", file=sys.stderr)
         if response.ok:
             due_tasks.extend(response.json()["tasks"])
         else:
             print("ClickUp error")
             sys.exit(1)
 
-    now = datetime.datetime.utcnow()
+    start_day = datetime.date.today() - datetime.timedelta(days=DAYS_BACK)
+    start_day_time = datetime.datetime.combine(start_day, datetime.datetime.min.time()).astimezone()  # local 00:00
     response = requests.get(
         "https://clickup-task-aggregator.herokuapp.com/get-completed",
         headers={"Authorization": config["task_aggregator_api_token"]},
-        params={"start": (now - datetime.timedelta(weeks=1)).isoformat(), "end": now.isoformat()}
+        params={"start": (start_day_time + OFFSET_INTO_NEW_DAY).isoformat(), "end": datetime.datetime.utcnow().isoformat()}
     )
+    print(".. aggregated tasks", file=sys.stderr)
     assert response.ok
     completed_tasks = response.json()
 
-    completed = len(completed_tasks)
+    per_day = [0] * (DAYS_BACK + 1)
+    tasks_of_day = [list() for _ in range(DAYS_BACK + 1)]
+    for task in map(models.CompletedTaskLog.parse_obj, completed_tasks):
+        delta = (task.timestamp.astimezone(pytz.utc) - OFFSET_INTO_NEW_DAY) - start_day_time
+        per_day[delta.days] += task.points
+        tasks_of_day[delta.days].append(task)
+
+    points_today = per_day[-1]
     due = len(due_tasks)
-    emoji = "sunglasses" if completed >= config["task_goal"] else "runner"
-    print(f"{completed} / {config['task_goal']}, <span color='lightblue'>due: {due}</span> :{emoji}: | iconName=object-select-symbolic")
+    emoji = "sunglasses" if points_today >= config["task_goal"] else "runner"
+
+    clearfix = "<span color='blue'> </span>"
+    green = "color='green'"
+    print(f"{clearfix}<span {green if points_today >= config['task_goal'] else ''}>{points_to_string(points_today)} / {config['task_goal']}</span>, "
+          f"<span color='orange'>due: {due}</span> :{emoji}: | iconName=object-select-symbolic")
     print("---")
-    termplot.plot([0, 1, 2, 3, 2, 1, 2, 3, 0, 2, 1, 2], plot_height=3)
+    print(f"{clearfix}<tt><b>" + " ".join(map(
+        lambda x: f"<span color='{'green' if x >= config['task_goal'] else 'orange'}'>{points_to_string(x)}</span>",
+        per_day
+    )) + "</b></tt>")
     if due > 0:
         print("---")
         for task in due_tasks[:6]:
             print(f"{task['name'][:30]} | iconName=mail-forward-symbolic href={task['url']}")
         if due > 6:
             print(f"... and {due - 6} more")
-    if completed > 0:
+    if points_today > 0:
         print("---")
-        for task in completed_tasks:
-            print(f"{task['task_name'][:30]} | iconName=object-select-symbolic href=https://app.clickup.com/t/{task['task_id']}")
+        for task in tasks_of_day[-1]:
+            print(f"{task.task_name[:30]} [{task.points}p] | iconName=object-select-symbolic href=https://app.clickup.com/t/{task.task_id}")
 
 
 if __name__ == '__main__':
